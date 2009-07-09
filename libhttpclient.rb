e633f666afad96ac0e7a515aba559b04057312ed
jj <none@none>
2009-07-09 16:19:30 +0200

    libhttpclient: current site == vhost

--- libhttpclient.rb
+++ libhttpclient.rb
@@ -231,7 +231,7 @@ class HttpClient
 #			puts "#{url} => 302 to #{newurl}" if newurl !~ /^[0-9a-zA-Z.:\/-]*$/ and page.status == 302 rescue nil
 			puts "#{url} => 301 to #{newurl}" if page.status == 301 rescue nil
 			case newurl
-			when /^http#{'s' if @http_s.use_ssl}:\/\/#{@http_s.host}(?::#{@http_s.use_ssl ? 443 : 80})?(.*)$/, /^(\/.*)$/
+			when /^http#{'s' if @http_s.use_ssl}:\/\/#{@http_s.vhost}(?::#{@http_s.vport or @http_s.use_ssl ? 443 : 80})?(.*)$/, /^(\/.*)$/
 				newurl = $1
 				if newurl =~ /^(.*?)\?(.*)$/
 					newurl, gdata = $1, $2
